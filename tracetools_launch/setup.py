2af02422d18214e6cd3e3f2204a3df8655654d92
Christophe Bedard <christophe.bedard@apex.ai>
2019-12-09 10:58:19 -0800

    Rebase from 'upstream'

--- tracetools_launch/setup.py
+++ tracetools_launch/setup.py
@@ -7,11 +7,13 @@ package_name = 'tracetools_launch'
 
 setup(
     name=package_name,
-    version='0.2.10',
+    version='0.2.11',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', glob.glob('launch/*.launch.py')),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
     ],
     install_requires=['setuptools'],
     maintainer=(
