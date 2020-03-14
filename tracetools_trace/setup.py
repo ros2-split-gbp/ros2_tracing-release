656ee6142f328a6d1fb0aaabe74a0458a95b701f
Christophe Bedard <bedard.christophe@gmail.com>
2020-03-14 11:51:34 -0400

    Imported upstream version '0.2.8' of 'upstream'

--- tracetools_trace/setup.py
+++ tracetools_trace/setup.py
@@ -5,17 +5,15 @@ package_name = 'tracetools_trace'
 
 setup(
     name=package_name,
-    version='0.2.12',
+    version='0.2.8',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
     ],
     install_requires=['setuptools'],
     maintainer=(
         'Christophe Bedard, '
-        'Ingo Luetkebohle'
+        'Ingo Lütkebohle'
     ),
     maintainer_email=(
         'bedard.christophe@gmail.com, '
