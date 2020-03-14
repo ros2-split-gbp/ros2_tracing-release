fff2b8f1e3e2cf559a20048762c37820178619df
Christophe Bedard <bedard.christophe@gmail.com>
2020-03-14 11:39:10 -0400

    Rebase from 'upstream'

--- tracetools_launch/setup.py
+++ tracetools_launch/setup.py
@@ -7,16 +7,18 @@ package_name = 'tracetools_launch'
 
 setup(
     name=package_name,
-    version='0.2.8',
+    version='0.2.12',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', glob.glob('launch/*.launch.py')),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
     ],
     install_requires=['setuptools'],
     maintainer=(
         'Christophe Bedard, '
-        'Ingo Lütkebohle'
+        'Ingo Luetkebohle'
     ),
     maintainer_email=(
         'bedard.christophe@gmail.com, '
