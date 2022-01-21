bae55777785146e36e56c3a5d1d70754c16c3aa1
Christophe Bedard <bedard.christophe@gmail.com>
2022-01-20 19:19:49 -0500

    Rebase from 'debian/rolling/tracetools_trace'

--- tracetools_trace/trace.py
+++ tracetools_trace/trace.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # Copyright 2019 Robert Bosch GmbH
+# Copyright 2021 Christophe Bedard
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -28,13 +29,14 @@ from tracetools_trace.tools import signals
 
 
 def init(
+    *,
     session_name: str,
     base_path: Optional[str],
     ros_events: List[str],
     kernel_events: List[str],
-    context_names: List[str],
+    context_fields: List[str],
     display_list: bool = False,
-) -> None:
+) -> bool:
     """
     Init and start tracing.
 
@@ -43,9 +45,11 @@ def init(
     or `None` for default
     :param ros_events: list of ROS events to enable
     :param kernel_events: list of kernel events to enable
-    :param context_names: list of context names to enable
+    :param context_fields: list of context fields to enable
     :param display_list: whether to display list(s) of enabled events and context names
+    :return: True if successful, False otherwise
     """
+    # Check if LTTng is installed right away before printing anything
     if not lttng.is_lttng_installed():
         sys.exit(2)
 
@@ -63,10 +67,10 @@ def init(
             print_names_list(kernel_events)
     else:
         print('kernel tracing disabled')
-    if len(context_names) > 0:
-        print(f'context ({len(context_names)} names)')
+    if len(context_fields) > 0:
+        print(f'context ({len(context_fields)} names)')
         if display_list:
-            print_names_list(context_names)
+            print_names_list(context_fields)
 
     if not base_path:
         base_path = path.get_tracing_directory()
@@ -75,17 +79,21 @@ def init(
 
     input('press enter to start...')
     trace_directory = lttng.lttng_init(
-        session_name,
+        session_name=session_name,
         base_path=base_path,
         ros_events=ros_events,
         kernel_events=kernel_events,
-        context_names=context_names,
+        context_fields=context_fields,
     )
+    if trace_directory is None:
+        return False
     # Simple sanity check
     assert trace_directory == full_session_path
+    return True
 
 
 def fini(
+    *,
     session_name: str,
 ) -> None:
     """
@@ -98,22 +106,24 @@ def fini(
 
     def _fini() -> None:
         print('stopping & destroying tracing session')
-        lttng.lttng_fini(session_name)
+        lttng.lttng_fini(session_name=session_name)
 
     signals.execute_and_handle_sigint(_run, _fini)
 
 
-def main():
+def main() -> int:
     params = args.parse_args()
 
-    init(
-        params.session_name,
-        params.path,
-        params.events_ust,
-        params.events_kernel,
-        params.context_names,
-        params.list,
-    )
+    if not init(
+        session_name=params.session_name,
+        base_path=params.path,
+        ros_events=params.events_ust,
+        kernel_events=params.events_kernel,
+        context_fields=params.context_fields,
+        display_list=params.list,
+    ):
+        return 1
     fini(
-        params.session_name,
+        session_name=params.session_name,
     )
+    return 0
