1662d8446a25152a86cbab518fe0197b8df6fff4
Chris Lalancette <clalancette@gmail.com>
2023-02-14 21:51:27 +0000

    Rebase from 'debian/rolling/tracetools_launch'

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ package_name = 'tracetools_launch'
 
 setup(
     name=package_name,
-    version='4.1.0',
+    version='5.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
@@ -26,7 +26,7 @@ setup(
     ),
     author='Christophe Bedard',
     author_email='fixed-term.christophe.bourquebedard@de.bosch.com',
-    url='https://gitlab.com/ros-tracing/ros2_tracing',
+    url='https://github.com/ros2/ros2_tracing',
     keywords=[],
     description='Launch integration for tracing.',
     long_description=(
